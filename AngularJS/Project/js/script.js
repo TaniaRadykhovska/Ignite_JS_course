0ef60b6a218aac03aa49fb51862537564489c652
Igor Zagnybida <zagnuk@hotmail.com>
2017-05-01 10:43:09 +0300

    Add new Project.

---
+++ AngularJS/Project/js/script.js
@@ -0,0 +1,81 @@
+
+var app = angular.module("appKet", ["smoothScroll"]);
+
+app.controller("mainCtrl", ["$scope", "smoothScroll", function($scope, smoothScroll){
+
+	$scope.menuItems = [
+		{ id: "top", name: "HOME"},
+		{ id: "services", name: "SERVICES"},
+		{ id: "portfolio", name: "PORTFOLIO"},
+		{ id: "about-us", name: "ABOUT"},
+		{ id: "news", name: "NEWS"},
+		{ id: "contact", name: "CONTACT"}
+	];
+
+	$scope.activeMenu = "top";
+
+	$scope.scrollTo = function(anchor) {
+
+		var options = {
+			duration: 700,
+			easing: 'easeInQuad',
+			offset: 100
+		},
+		elem = document.getElementById(anchor);
+		if ( elem ) {
+			smoothScroll(elem, options);
+		}			
+	}
+
+	$scope.activateMenu = function(anchor) {
+		$scope.activeMenu = anchor;
+	}
+}]);
+
+app.controller("serviceCtrl", ["$scope", function($scope) {
+	$scope.activeService = "web";
+	$scope.serviceItems = [
+		{name: "WEB DESIGN", class: "web", blockL: false},
+		{name: "GRAPHIC DESIGN", class: "graphic", blockL: false},
+		{name: "PROGRAMMING", class: "programming", blockL: false},
+		{name: "PHOTOGRAPHY", class: "photography", blockL: true}
+	]
+	$scope.viewService = function(service) {
+		$scope.activeService = service;
+	}
+}]);
+
+app.controller("registerFormCtrl",["$scope", function($scope) {
+	$scope.nameRegex = /^[a-zA-Z]*$/;
+	$scope.mailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]+$/; 
+	$scope.passwordRegex = /.{20,}/;
+
+	$scope.submitHandler = function(isvalid) {
+		if (isvalid) {
+			alert('Спасибо! Ваша регистрация принята!!!)');
+		}
+		else {
+			alert('Please enter your data');
+		}
+	}
+	$scope.showError = function (err) {
+        if (angular.isDefined(err)) {
+            if (err.required) {
+                return 'no data entered!'
+            }
+            else if (err.pattern) {
+                return "invalid data!";
+            }
+        }
+    }
+}]);
+
+app.controller("newsCtrl", function($scope, $http) {
+	$http.get("data.json").success(function (response) {
+		$scope.items = response;
+		for (var i in $scope.items) {
+	        $scope.items[i].date = new Date($scope.items[i].date);
+	    }
+  });
+
+});
